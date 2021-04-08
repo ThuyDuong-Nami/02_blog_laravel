54eb03c8bfcb190db5c2fda2627aebf90de0b636
ThuyDuong-Nami <kieuthuy.duong@staff.gumiviet.com>
2021-04-08 13:43:33 +0700

    feat: mapping column

--- app/Http/Controllers/API/FileController.php
+++ app/Http/Controllers/API/FileController.php
@@ -14,9 +14,7 @@ class FileController extends Controller
         if (file_exists($filepath)){
             $file = @fopen($filepath, 'r');
             $dataFile = fread($file, filesize($filepath));
-            return response()->json([
-                $dataFile
-                ], 200);
+            return response()->json($dataFile, 200);
             fclose($file);
         }else{
             return response()->json([
@@ -24,6 +22,7 @@ class FileController extends Controller
             ], 422);
         }
     }
+
     public function readCSV()
     {
         $filepath = 'storage/test.csv';
@@ -32,9 +31,43 @@ class FileController extends Controller
             while (($dataFile = fgetcsv($file)) !== false){
                 $csv[] = $dataFile;
             }
+            return response()->json($csv, 200);
+            fclose($file);
+        }else{
             return response()->json([
-                $csv,
-            ], 200);
+                'message' => 'File không tồn tại'
+            ], 422);
+        }
+    }
+
+    public function mappingColumn($csv)
+    {
+        $format = [
+            'username',
+            'email',
+            'password',
+        ];
+        for ($i = 0; $i < count($csv); $i++){
+            for ($j = 0; $j < count($format); $j++){
+                if ($i == $j){
+                    $csv[$i] = $format[$j];
+                }
+
+            }
+        }
+        return $csv;
+    }
+
+    public function formCSV()
+    {
+        $filepath = 'storage/test.csv';
+        if (file_exists($filepath)){
+            $file = @fopen($filepath, 'r');
+            while (($dataFile = fgetcsv($file)) !== false){
+                $csv[] = $dataFile;
+            }
+            $csv[0] = $this->mappingColumn($csv[0]);
+            return response()->json($csv, 200);
             fclose($file);
         }else{
             return response()->json([
--- routes/api.php
+++ routes/api.php
@@ -33,3 +33,4 @@ Route::group(['middleware' => 'api'], function (){
 Route::apiResource('post', API\PostResourceController::class);
 Route::get('file', [API\FileController::class, 'readFile']);
 Route::get('file/csv', [API\FileController::class, 'readCSV']);
+Route::get('file/formcsv', [API\FileController::class, 'formCSV']);
