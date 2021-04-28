98c4c1eac2da2e55bcaed567ab0e16ad38c0dab8
ThuyDuong-Nami <kieuthuy.duong@staff.gumiviet.com>
2021-04-28 13:44:27 +0700

    feat: schedule send mail

---
+++ app/Console/Commands/MailCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use App\Notifications\UserNotification;
+use Illuminate\Console\Command;
+
+class MailCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'mail:send';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send mail to users';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $users = User::all();
+        foreach ($users as $user){
+            $user->notify(new UserNotification());
+        }
+    }
+}
